17057299e10f51913d6db4281e28d20401d5bf83
Anthony Tedde <antho.tedde@gmail.com>
2017-06-15 14:30:29 +0200

    refactoring

--- Chapter3/SymmetricRandomWalk/SymmetricRandomWalk.R
+++ Chapter3/SymmetricRandomWalk/SymmetricRandomWalk.R
@@ -1,185 +1,93 @@
+##
+#
+# Chapter 3 > 3.2 Scaled symmetric random walk
+#
+# TODO: section: Create an array of (3000) steps sample random walk
+# Work to find a solution for functionnal way of doing the stuff
+#
+##
+
+#
+# Library
+#
+library(ggplot2)
+
+#
 # Probability measure:
+#
+# For the purpose of this theory, first are define the probabilities p and q such as it has the same occurence (one half)
+#
 p <- 1/2
 q <- 1 - p
-size <- 300
-spread <- 3000
+
+#
+# Some graphics will be created inside this folder:
+# code\Chapter3\figure
+#
+# To access the content of this folder, the variable figure could be used for:
+#
+ figure <- paste(getwd(), 'Chapter3', 'figure', sep = '/')
+
+#
+# Definition of the symmetric random walk
+#
+# The symmetric random walk is first defined by some samples (which makes an array of sampled symmetric random walk)
+# Variables:
+#   * [k]: The size of the sample is denoted by the variable k -> (see theory $M_k$ denote the Symmetric Random Walk variable)
+#   * [asize]: length of the array containing the Symmetric Random Walks
+#
+# Next it will be defined by its distributions.
+#
+# Both will be use full for finding expectation and variance theoterically or empirically
+#
+k <- 300
+asize <- 3000
 M <- 0
-### 
-# Mt and fi are dataframe with size = (size + 1) * (size + 1)
-# All value inside Mt and fi are zero.
-# check ate Initialisation section for size value.
-### 
-Mt <- fi <- data.frame(matrix(rep(0, (size + 1)^2), nrow = size + 1))
-l <- 300
-k <- 200
-##
-# Generation
-##
+
+###################################
+# Empirical Symmetric Random Walk #
+###################################
+#
+# Construction on the Empirical Random variable X
 X <- sample(x = c(-1, 1),
-            size = size,
+            size = k,
             replace = T,
             prob = c(p, q)
 )
-# Following lines must be changed into apply type function:
-for(i in 1:size) 
-  M[i + 1] <- sum(X[1:i]) # i + 1 because M[0] from theory is indeed represented by M[1] in this code.
-# Same result with apply type function:
-M_k <- c(0, 
+# 
+# Construction of the Empirical Random Walk
+#
+Mk <- c(0, 
          sapply(seq_along(X), function(x){sum(X[1:x])}))
-
 
 ##
 # Check by plot
 ##
-plot(M_k,
+file <- paste(figure, 'EmpiricalSymmetricRandomWalkSample.pdf', sep = '/')
+pdf(file = file)
+plot(Mk,
      type = 'l')
+dev.off()
 
-
-################################################################################
-# Creation of a 300 steps random walk
-################################################################################
-##
-# Construction of the theoretical Random Variable M based on the filtration F(t)
-# See document "randomWalkDistributionAnalysis.jgp"
-##
-# Usual wrong way:
-# Profiling give 1200 Ms
-for(j in 1:(size + 1))
-  for(i in 1:j)
-    Mt[i,j] <- (j-i) + (1-i)
-
-# Functional Way:
-# No information from profiler: too fast
-dim_x <- dim_y <- 1:(size + 1)
-o <- outer(dim_x, 
-           dim_y, 
-           FUN=function(r,c){(r-c) + (1-c)} )
-to <- t(o)
-subset <- upper.tri(to, diag = T)
-Mk <- to * subset
-# Better functional way:
-dim_x <- dim_y <- 1:(size + 1)
-o <- outer(dim_x, 
-           dim_y, 
-           FUN=function(r,c){ifelse(c>=r, (c-r) - (r-1), NA_integer_)}) 
-
-
-##
-# Construction of the distribution of the theoretical random variable
-##
- # Usual wrong way:
-# Profiling give 1200 Ms
-for(j in 1:(size+1))
-  for(i in 1:j)
-    fi[i, j] <- choose((j-1), 
-                       (j-i)) * p^(j-1)
-
-# Same results following funcional way
-fi <- outer(dim_x,
-           dim_y,
-           FUN = function(i, j){choose((j-1), (j-i)) * p^(j-1)})
-
-##
-# Graph of the Theoretical distribution
-##
-
-range <- 1:ncol(Mk)
-lastToss <- ncol(Mk)
-# Using plot
-plot(Mk[range, lastToss],
-     fi[range, lastToss], 
-     type = 'l', 
-     xlim = c(-75, 75))
-# Using ggplot
-# data.frame which map distribution and random variable:
-distributionSymRanWal <- data.frame(
-  Value = Mt[range, lastToss],
-  Frequency = fi[range, lastToss]
-)
-
-# For the sake of visibility the limit of X axis has been set to [-100, 100]
-library(ggplot2)
-ggplot(data = distributionSymRanWal, aes(Value, Frequency)) +
-  geom_line() +
-  scale_x_continuous(limits = c(-100, 100))
-
-
-
-
-################################################################################
-# Check the martingale property of symmetric random walk.
-################################################################################
+######################################################
+# Create an array of (3000) steps sample random walk #
+######################################################
 #
-# With l > k, show that:
-# E[Ml|F(k)] = Mk
-
-##
-# first: simple NOT CONDITIONAL Expectation
-# E[M200] should equal zero because:
-# M0 = 0 and Because Mt is a martingale thus:
-# E[M200|f(0)] = E[M200] - 0
-##
-EM200 = sum(Mt[1:200, 200] * fi[1:200, 200]) # equal zero.
-
-##
-# Expectation of Mt_l at k
-# denoted by: E[Mt_l|f(k)], with k < l
-##
-##
-# Variables
-##
-from <- 2 # Departure of the Expectation
-k <- 4 # To get the filtration point 
-l <- 19 # Give the period to be expected
-if(k>l) 
-interval <- l-k
-##
-# Partionated Symmetric Random Walk
-##
-# first the value of M at time $k$ has to be set. It means that the value at this time $k$ is not yet random but resolved.
-# Therefore at time $k$ the variable $M_k$ is not random. 
-# We can take any value we want to start with from 1 to $k + 1$.
-# I choose to name this variable $from$:
-from <- 2 # Departure of the Expectation
-# The lenght of the path is already know: $(l - k)$:
-len <- l - k
-i <- from:(from + len)
-j <- k:l
-df <- Mk[i, j]
-# names(df) <- sapply(1:ncol(df), function(x){paste0("X",x)})
-##
-# Con 
-##
-
-# The probability from k to l start from k to l. The probability table has therefore to be taken from begining.
-fi_min <- fi[1:(len+1), 1:(len+1)]
-# Old calculation of fi:
-# 
-# fi <- data.frame(matrix(rep(0, (l-k + 1)^2), nrow = l-k + 1))
-# for(j in 1:(l-k+1))
-#   for(i in 1:j)
-#     fi[i, j] <- choose((j-1), (i-1)) * p^(j-1)#((j-1)*p^(j-1))/(factorial(j-1)*factorial(1+i))
-# Finally compute the expectation E[Mt_l|f(k)]:
-Mk[from, k]
-sum(df[, l-k+1] * fi_min[, l-k+1]) #Yeah it is a matringale
-
-########################################
-# Create a 300 steps random walk
-########################################
 # Empirical distribution based on a series of outcomes of symmetric random walk
-k <- size <- 3000
+k <- 3000
 # n is the number of case
-n <- spread <- 300000
+asize <- 300000
 Xs <- list()
-for(i in 1:spread)
+for(i in 1:asize)
   Xs[[i]] <- sample(x = c(-1, 1),
                     replace = T,
                     prob = c(p, q),
-                    size = size)
-lapply(Xs, sample, x = c(-1,1), replace = T, prob = c(p, q), size = k)
+                    size = k)
+lapply(1:2, FUN = function(x){})
+list()
 
 Wn <- vector()
-for(i in 1:spread)
+for(i in 1:asize)
   Wn[i] <- sum(Xs[[i]])
 
 hist(Wn)
@@ -195,6 +103,117 @@ Ex <- weighted.mean(Xk, c(p, q))
 Ex.square <- weighted.mean(Xk^2, c(p, q))
 # Variance
 S <- Ex.square - Ex^2
+
+ 
+############################ 
+# Theoretical distribution #
+############################ 
+#
+#  *[Mt]: Stands for Theoretically Symm. Random Walk
+# 
+# The array (data.frame) used to store all the value that the variable Mt could take according to the path has a dimension dim_x * dim_y
+dim_x <- dim_y <- 1:(k + 1)
+Mt <- outer(dim_x, 
+           dim_y, 
+           FUN=function(r,c){ifelse(c>=r, (c-r) - (r-1), NA_integer_)}) 
+
+#
+# Mt contains all the values the random variables could take
+# it remains to derive all the probabilities associated to that values:
+#
+#   * [fi]: Dataframe which has same dimension than Mt and which gives the probability associated.
+#
+fi <- outer(dim_x,
+           dim_y,
+           FUN = function(i, j){choose((j-1), (j-i)) * p^(j-1)})
+
+##
+# Graph of the Theoretical distribution
+##
+#
+#   * [DistributionSymRanWal]: data.frame wich contains two columns:
+#     + [Value] All the possible value that the random walk could take at time = k (after k coin tosses)
+#     + [Frequency] The associated probability
+#   * [range]: Number of possibility of value for the random variable at t = k
+#   * [lastToss]: Only the last coin toss is interesting to graph the distribution
+#
+range <- dim_x
+lastToss <- length(dim_y)
+
+distributionSymRanWal <- data.frame(
+  Value = Mt[range, lastToss],
+  Frequency = fi[range, lastToss]
+)
+
+# For the sake of visibility the limit of X axis has been set to [-100, 100]
+file <- paste(figure, 'SymmetricRandomWalkDistribution.pdf', sep = '/')
+
+pdf(file = file)
+
+ggplot(data = distributionSymRanWal, aes(Value, Frequency)) +
+  geom_line() +
+  scale_x_continuous(limits = c(-100, 100))
+
+dev.off()
+
+###########################################################
+# Check the martingale property of symmetric random walk. #
+###########################################################
+#
+# With l > k, show that:
+# E[Ml|F(k)] = Mk
+
+##
+# first experiment: 
+# simple NOT CONDITIONAL Expectation
+# E[M200|f(0)] should equal zero because:
+# M0 = 0 and Because Mt is a martingale thus:
+##
+EM200 = sum(Mt[1:200, 200] * fi[1:200, 200]) # equal zero.
+
+##
+# Next experiment: Conditional expectation
+# It means: Expectation of Mt_l at k
+# denoted by: E[Mt_l|f(k)], with k < l
+#
+# Variables
+#   * [from] : Because the random variable could take more than one value at t = k, a starting point for expectation must be fixed
+#   * [k] : to fix the filtration point F(k)
+#   * [l] : Indice of the random variable on which the expectation will be computed
+#   * [len]: length between k and l
+#   * [df] : reduced data.frame containing data useful for calculation of expectation
+#   * [i] : Row index of the reduce data.frame which take only the values useful for the calculation of expectation
+#   * [j] ; Such as j but using as column index.
+#   * [fi_min] : Shorter version of the probabilities' array used above for Mk variable.
+##
+# To make another calculation of other expectation the following variables should be changed:
+#   * [from] : as starting column point
+#   * [k] : As stating row point
+#   * [l] : Ending column point. (the ending x-axis point in automatically given by the procedure.)
+##
+from  <- 2
+k     <- 4
+l     <- 19
+#
+# IMPROVE:
+# Furhter release of the document should deal with environment
+# It could be possible to use lockBinding to make the following variable acting as constant.
+#
+len     <- abs(l - k)
+i       <- from:(from + len)
+j       <- k:l
+df      <- Mt[i, j]
+fi_min  <- fi[1:(len+1), 1:(len+1)]
+#
+# Finally compute the expectation E[Mt_l|f(k)]:
+# Does the starting value equal the expectation up to l ?
+#   * [Mt[from, k]]: (from, k) are the coordinate of the starting point from where the calculation of the expectation start.
+#
+ifelse(Mt[from, k] == sum(df[, l-k+1] * fi_min[, l-k+1]),
+       "martingale!",
+       'Not a martingale')
+ 
+
 
 
 
