36e77295beee4ac50e129b24f85d55435e457b9d
Anthony <antho.tedde@gmail.com>
2017-06-23 22:27:39 +0200

    Replication the short position in the one period binomial model

--- StochasticCalculusForFinanceVol1/Chapter1_BinomialNoArbitragePricingModel/1.1_OnePeriodBinomialModel.R
+++ StochasticCalculusForFinanceVol1/Chapter1_BinomialNoArbitragePricingModel/1.1_OnePeriodBinomialModel.R
@@ -4,17 +4,25 @@
 #                           #
 #############################
 #
-#   * [t0] : Beginning of the period
-#   * [t1] : End of the periodod
-#   * [S0] : Price of the stock at t0 (Deterministic variable)
-#   * [S1] : Price of the stock at t1 (Random variable) can take 2 states:
-#     + [S1H] : If coin toss results to HEAD
-#     + [S1T] : If coin toss results to TAIL
-#   * [u] : Up-factor
-#   * [d] : Down-factor. Could not be less that 0 because otherwise
-#           There is an arbitrage opportunity
-#   * [r] : Interest rate. Could not be less that -1. See rule 1.1.2
-#           To understand why.
+#   * [t0]: Beginning of the period
+#   * [t1]: End of the periodod
+#   * [S0]: Price of the stock at t0 (Deterministic variable)
+#   * [S1]: Price of the stock at t1 (Random variable) can take 2 states:
+#     + [S1H]: If coin toss results to HEAD
+#     + [S1T]: If coin toss results to TAIL
+#   * [u]: Up-factor
+#   * [d]: Down-factor. Could not be less that 0 because otherwise
+#          There is an arbitrage opportunity
+#   * [r]: Interest rate. Could not be less that -1. See rule 1.1.2
+#          To understand why.
+#   * [k]: Strike price
+#
+#   Unknowns of the model (Nevertheless for the sake of example 1.1.1,
+#   some value has been set for those unknowns):
+#
+#   * [X0]: Intial wealth
+#   * [delta0]: # of share of stock that one must hold to replicate
+#     the position.
 #
 # (!) We do not assume that the coin is faire. It means that the probability
 # of head must not be the same as the probability of tails.
@@ -44,12 +52,15 @@ runArbitrage_babyRun <- function(d, u, r){
 ############################
 # Variables Initialisation #
 ############################
-t0 <- 0
-t1 <- 1
-S0 <- 4
-u <- 2
-r <- 1/4
-p <- 0.5
+t0      <- 0
+t1      <- 1
+S0      <- 4
+k       <- 5
+u       <- 2
+r       <- 1/4
+p       <- 0.5
+X0      <- 1.20
+delta0  <- 0.5
 
 ##
 # Computed value - Do not touch it even if you are a Jedi
@@ -114,21 +125,130 @@ if(
 #               of losing money, and has a positive probability of making money.
 
 
+############################
+# Replication take place   #
+############################
+#
+# Some definitions:
+#
+#   * European call option: Options which confers on its owners the right
+#       but not the obligation to buy one share of stock at time one for 
+#       a predetermined price called the strike price.
+#
+#   * European put option: Options which confers on its owners the right  
+#       but not the obligation to sell one share of stockt at time one for
+#       a predertermined price called the stroke.
+#
+#   * European vs. American: The only difference between these kind of options
+#       is that the European option can only be exercised at maturity whereas
+#       an American one could be exercised at any time before maturity.
+#
+# The position in option also are important to be mentioned:
+#
+#   * Long position: Buyer of the option. In this position one purchase the
+#       right but not the obligation to exercise it.
+#
+#   * Short position: Seller of the option. In this position one must sell or
+#       buy the underlying according to the fact that the long position
+#       choose to exercises its right or not.
+#
+# arbitrage trading theory is an approach of the option-pricing problem that
+# consists on replicate the option by trading in the stock and money market.
+#
+# An example using the values defined just above.
+# Reminder of the value associated to the different variables:
+#     * t0      <- 0
+#     * t1      <- 1
+#     * S0      <- 4
+#     * k       <- 5
+#     * u       <- 2
+#     * r       <- 1/4
+#     * p       <- 0.5
+#     * X0      <- 1.20
+#     * delta0  <- 0.5
 
+#
+# Gain in the case the stock price goes to head at T1
+#
+# Variables
+#   * [InitialPortFolioValue]: The value of the portfolio at T0 (Not random)
+#   * [LongPosition]: The long position in the option.
+#                     (Mean the holder of the right over the underlying)
+#     * ['H']: If the coin toss result to head
+#     * ['T']: If the coin toss result to tail
+#   * [ShortPosition]: The short position in the option.
+#                       (Concerne the one which hold a claim)
+#     * ['H']: If the coin toss result to head
+#     * ['T']: If the coin toss result to tail
+#   * [MoneyMarketValue]: The growth of the value invested / borrowed
+#                         to the money market
+#     * [+]: The value is positive if one invests in MM
+#     * [-]: The value is negative if one borrows in MM
+#   * [StockHoldingMarketValue]: Value of the stock according to the delta
+#                                 (The amount hold by the hedger)
+#     * ['H']: If the coin toss result to head
+#     * ['T']: If the coin toss result to tail
+#   * [PortfolioValue]: The value of the portfolio at T1 except the value of the
+#                       stock option hold or owe.
+#     * ['H']: If the coin toss result to head
+#     * ['T']: If the coin toss result to tail
+#
+LongPosition <- c('H' = max(S1H - k, 0),
+                  'T'= max(S1T - k, 0))
+ShortPosition <- c('H' = min(k - S1H, 0),
+                   'T' = min(k - S1T, 0))
+#
+# If a head occurs:
+#   -> The short position value is -3. Thus to replicate the position we must do
+#       a trade that provide 3 at T1
+# If a tail occurs:
+#   -> The short position value is 0. Thus to replicate the position we must do
+#       a trade that provide 0 at T1
+#
 
+############################
+# hedge of short position  #
+############################
 
+# At time zero the value of the portfolio is:
+#  (This is the only value which is not random)
+#
+InitialPortfolioValue <- X0 - delta0 * S0
 
+# At Time one if the coin toss results to head/tail we get 
+# (as holder of short position):
+MoneyMarketValue <- InitialPortfolioValue * (1 + r)
+StockHoldingMarketValue <- c('H' = delta0 * S1H,
+                             'T' = delta0 * S1T)
 
+# For the following code the name 'H' and 'T' has not been set up
+# especially to not duplicate the information inside the vector [PortfolioValue]
+#
+PortfolioValue <- c(MoneyMarketValue + StockHoldingMarketValue['H'],
+                    MoneyMarketValue + StockHoldingMarketValue['T'])
 
+#
+# If following check is TRUE then the short position has been replicated:
+#
+if ( identical(PortfolioValue, -1 * ShortPosition) )
+  print(paste('Ok the short position has been replicated and the value', 
+               'of the option at T0 should be fixed at',
+               paste0('$',X0),
+               'to avoid arbitrage', sep = ' ')) else
+  print('Fuck off, the computation lead to a universe black hole...')
+#
+# The previous check verify the hedge and return:
+# 
+# [1] "Ok the short position has been replicated and the value of the option 
+#     at T0 should be fixed at $1.2 to avoid arbitrage"
+#
 
-
-
-
-
-
-
-
-
+############################
+# hedge of long position   #
+############################
+#
+# To hedge the long position one have only to do the reverse of the previous
+# strategy
 
 
 
